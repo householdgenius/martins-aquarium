beb0954393193437a03cdbe8b75da943de48d413
householdgenius <emily.reed0913@gmail.com>
2021-08-19 11:00:22 -0500

    added some js

---
+++ FishData.js
@@ -0,0 +1,96 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete this
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const myName = "Emily";
+let myFavoriteColor = "Purple";
+
+const fishCollection = [
+    {
+        name: "Bart",
+        food: "crustaceans",
+        image: "blue.jpg",
+        species: "trigger",
+        length: 7,
+        waterType: "salt", 
+        harvestLocation: "Costa Rica"
+
+    },
+    {
+        name: "Mr Freeze",
+        food: "Meal Worms",
+        image: "red.jpg",
+        species: "tang",
+        length: 3,
+        waterType: "salt", 
+        harvestLocation: "Florida"
+
+    },
+    {
+        name: "Karen",
+        food: "Algae",
+        image: "yellow.jpg",
+        species: "Siamese algae eater",
+        length: 5,
+        waterType: "salt", 
+        harvestLocation: "Puerto Rico"
+
+    },
+    {
+        name: "Nemo",
+        food: "Goldfish",
+        image: "blue.jpg",
+        species: "Clown",
+        length: 3,
+        waterType: "Salt-water",
+        harvestlocation: "Florida"
+    },
+    {
+        name: "Don Julio",
+        food: "snails",
+        image: "stingray.jpg",
+        species: "Stingray",
+        length: 9,
+        waterType: "Salt",
+        harvestLocation: "Dominican Republic"
+
+    },
+    {
+        name: "Robin Gilliams",
+        food: "Bee Moths",
+        image: "orange.jpg",
+        species: "Clownfish",
+        length: 2,
+        waterType: "Salt",
+        harvestLocation: "Mexico"
+    },
+    {
+        name: "Bruce",
+        food: "not fish",
+        image: "bruce.jpg",
+        species: "greatWhite",
+        length: 130,
+        waterType: "salt",
+        harvestLocation: "Sydney"
+    },
+    {
+        name: "Bloat",
+        food: "shrimp",
+        image: "yellow.jpg",
+        species: "Blow Fish",
+        length: 5,
+        waterType: "Salt",
+        harvestLocation: "Mexico"
+    },
+    {
+        name: "Gordon",
+        food: "Krill",
+        image: "pink.jpg",
+        species: "Blowfish",
+        length: 3,
+        waterType: "Salt",
+        harvestLocation: "Asia"
+    }
+]
